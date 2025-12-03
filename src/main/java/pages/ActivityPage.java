4e8ff5175ecfd8494080585cc7522a78c755fb94
nakonechnyy.i <ihor.nakonechnyy@nixs.com>
2025-12-03 15:38:12 +0100

    ADU_1 done

--- src/main/java/pages/ActivityPage.java
+++ src/main/java/pages/ActivityPage.java
@@ -4,6 +4,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
+
 
 import java.time.Duration;
 
@@ -16,14 +18,50 @@ public class ActivityPage {
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
     }
 
+    @FindBy(id = "accountId")
+    WebElement accountID;
+    @FindBy(id = "accountType")
+    WebElement accountType;
+    @FindBy(id = "balance")
+    WebElement balance;
+    @FindBy(id = "availableBalance")
+    WebElement availableBalance;
+    @FindBy(id = "month")
+    WebElement ActivityPeriodFilter;
+    @FindBy(id = "transactionType")
+    WebElement transactionTypeFilter;
+
+
     @FindBy(xpath = "//table[@id='transactionTable']//tr[1]/td[2]")
     WebElement transactionTableFirstTransaction;
 
+    public boolean accountIDIsDisplayed() {
+        accountID.isDisplayed();
+        return true;
 
+    }
+    public boolean accountTypeIsDisplayed() {
+        accountType.isDisplayed();
+        return true;
+    }
+    public boolean balanceIsDisplayed() {
+        balance.isDisplayed();
+        return true;
+    }
+    public boolean availableBalanceIsDisplayed() {
+        availableBalance.isDisplayed();
+        return true;
+    }
+    public String isAllSelectedByDefault() {
+        Select select = new Select(ActivityPeriodFilter);
+        return select.getFirstSelectedOption().getText().trim();
 
-    public void openFirstTransaction() {
-        transactionTableFirstTransaction.click();
     }
 
+
+    public boolean openFirstTransaction() {
+        transactionTableFirstTransaction.click();
+        return true;
+    }
 
 }
---
+++ src/test/java/ADU/ADU_1.java
@@ -0,0 +1,55 @@
+package ADU;
+
+import com.beust.ah.A;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.*;
+import pages.*;
+
+import java.time.Duration;
+
+public class ADU_1 {
+    private WebDriver driver;
+    private HomePage homePage;
+    private ActivityPage activityPage;
+    private TransactionPage transactionPage;
+    private AccountsOverviewPage accountsOverviewPage;
+
+    @BeforeMethod
+    public void setDriver() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-features=PasswordChangeDetection,PasswordLeakDetection,AutofillKeyedPasswords");
+        options.addArguments("--incognito");
+        options.addArguments("--disable-popup-blocking");
+
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(1));
+        driver.get("http://localhost:8080/parabank/index.htm");
+
+        homePage = new HomePage(driver);
+        activityPage = new ActivityPage(driver);
+        transactionPage = new TransactionPage(driver);
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        if (driver != null) {
+            //driver.quit();
+        }
+    }
+
+    @Test
+    public void transactionDetailsTest() {
+        homePage.userLogIn("parasoft", "demo");
+        homePage.openFirstAccount();
+
+        Assert.assertTrue(activityPage.accountIDIsDisplayed(), "Account Number is not displayed");
+        Assert.assertTrue(activityPage.accountTypeIsDisplayed(), "Account Type   is not displayed");
+        Assert.assertTrue(activityPage.balanceIsDisplayed(), "Balance is not displayed");
+        Assert.assertTrue(activityPage.availableBalanceIsDisplayed(), "Available Balance is not displayed");
+
+    }
+}
